34746dc706fb8035cc74a2188147f5faa8a9743a
2pelaezenrique <2pelaezenrique@gmail.com>
2015-12-15 06:01:18 +0100

    Añadido boton publicar, añadidos campos al modelo Material y comenzado su formulario

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,10 +2,17 @@ class Material
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
+  field :format, type: String
   # field :uploadDate, type: Date
   field :type, type: String
   # field :category, type: Hash
-  field :format, type: String
+  field :link, type: String
+  field :authors, type: Array
+  field :youtubeChannel, type: String
+  field :account, type: String
+
+  field :tags, type: Array
+  field :subject, type: String
   belongs_to :user
   embeds_many :comments
 end
