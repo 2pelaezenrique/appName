f5c8062b4dad68384f244def1a34a59c3a98a1e5
2pelaezenrique <2pelaezenrique@gmail.com>
2016-01-27 09:23:53 +0100

    añadida redireccion para el logout y el update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     session[:previous_url] || root_path
   end
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
+  end
+  def after_update_path_for(resource)
+    session[:previous_url] || root_path
+  end
   protected
   
   def configure_permitted_parameters
