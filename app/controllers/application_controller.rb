62cce71cb2b564aab549c18069c81d1c2fb2a0c7
2pelaezenrique <2pelaezenrique@gmail.com>
2016-01-27 09:22:08 +0100

    Redireccion tras login y sign out añadida

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,31 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :store_location
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token 
 
   Yt.configuration.api_key = ENV["YOUTUBE_API_KEY"]
   Yt.configure do |config| 
     config.log_level = :debug 
+  end
+  private
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        request.env["HTTP_X_REQUESTED_WITH"] != "XMLHttpRequest" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
   protected
   
