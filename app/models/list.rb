747158c0939b0444928e562201e2f040b3850175
2pelaezenrique <2pelaezenrique@gmail.com>
2016-02-01 18:01:11 +0100

    Ahora se muestran las listas con un icono de check

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,9 @@
 class List
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  belongs_to :user
-  field :name, type: String
-  field :description, type: String
-  field :public, type: Mongoid::Boolean
-  field :materials, type: String
+	include Mongoid::Document
+	include Mongoid::Timestamps
+	belongs_to :user
+	field :name, type: String
+	field :description, type: String
+	field :public, type: Mongoid::Boolean
+	field :materials, type: Array, default: []
 end
