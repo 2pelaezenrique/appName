ec4f28092d7d11be56ed8cce3b0e9c51e9706835
2pelaezenrique <2pelaezenrique@gmail.com>
2016-06-15 02:13:41 +0200

    Añadidos objetivos

---
+++ objetivos.rb
@@ -0,0 +1,132 @@
+# Poder: ...
+
+
+
+############# Materiales ######################
+# Subir material con una descripcion breve y ciertas referencias
+# Clasificar el material por tema
+# Añadir el material a una determinada escuela
+# Ver videos de youtube
+# Ver videos subidos
+# Ver pdf
+# Ver fotos
+# Mostrar todos los demas tipos de archivos para poder descargarlos
+# Buscar un material
+# Filtrar la busqueda por escuela
+# Filtrar la busqueda por autor
+# Filtrar la busqueda por tema
+# Ordenar la busqueda por favs / fecha
+# Mostrar un top
+# ....
+
+
+
+
+################### Usuario #####################
+# Tener una cuenta de usuario
+# Tener un avatar
+# Tener datos personales
+# Tener CV
+
+# Añadir a favoritos un material
+# Añadir a una lista de reproduccion un material
+# Comentar un material
+
+# Ver tabla con todos los favoritos
+# Ver tabla con los elementos de una lista
+# Reordenar una lista
+
+# Crear una asignatura
+# Pertenecer a una asignatura
+
+# Disponer de diferentes diseños
+# Cambiar a diseño de estudiante
+# Cambiar a diseño de creador de contenido
+# Cambiar a diseño de profesor
+# Cambiar a diseño de administrador de escuela
+
+# Tener notificaciones
+# Ver un calendario
+# Enviar mensaje a otro usuario 
+
+# Ver aplicaciones que has autorizado 
+
+
+
+
+############### Asignatura ##########################
+
+# Elegir si es publica, con contraseña o privada
+
+# Crear un apartado
+# Añador materiales al apartado
+# Añadir un ejercicio a una apartado
+# Añadir una entrega a un apartado
+# Añadir una encuesta a un apartado
+# Ocultar un elemento de un apartado
+# Mostrar a unos estudiantes determinados
+# Añadir contenidos especificos para cada usuario (Examenes escaneados, ejercicios personalizados, ... )
+
+# Registrar usuarios en el aula
+# Crear una tabla de calificaciones para la asignatura
+# Calificar a un alumno
+
+# Asociar un foro a la asignatura
+# Añadir eventos
+
+
+
+############## Ejercicios  y entregas #####################
+
+# Fecha de apertura
+# Tiempo de ejercicio
+# Fecha de cierre
+# Maximas entregas
+# Crear preguntas
+# Tener diferentes tipos de preguntas, test, formulario, etc
+# Subir documento 
+# Comentar el ejercicio
+
+
+
+
+################ Visionado de datos #####################
+
+# Mostrar en tabla a todos tus alumnos
+# Agrupar por carpetas materiales / alumnos
+# Manipular una carpeta en vez de un solo usuario
+
+
+
+
+################ Alumnos ##################################
+
+# Poder enviar mensaje
+# Poder ordenar por calificacion
+# Enviar mensaje automatico con sus notas
+# Registrarlos en una asignatura
+
+
+
+
+################ API ######################################
+		####### Punto de vista del servidor ########
+
+# Instalación sencilla
+# Plugins personalizados
+# Avisos de actualización
+
+		####### Punto de vista del Usuario ########
+
+# Poder autorizar otro usuario de otro appName
+# Sistema de tokens, Ej: Tu aplicacion se registra en el appName de la upm, tienes unos tokens, y los usuarios pueden iniciar sesion a traves de upm
+# Ver por pestañas los diferentes appName
+# Todas las acciones anteriores a traves de una api
+
+
+
+
+######### Funciones internas de la aplicacion #############
+# Registrar una lista de emails, nombres , etc. 
+# Asociar dicha lista por grupos 
+# Correos automáticos (listas de distribución)
