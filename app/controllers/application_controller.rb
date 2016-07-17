ce26717bbcf27f7b233083527b0e25de98554748
2pelaezenrique <2pelaezenrique@gmail.com>
2016-07-17 11:47:50 +0200

    Actualizado a la ultima version de devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
